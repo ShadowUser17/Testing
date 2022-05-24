fa10d63051e968bb84a2ad4e900798259fd0c3c9
ShadowUser17 <linuxforce17@gmail.com>
2022-05-24 15:01:36 +0300

    Changes to be committed: 	modified:   .gitignore 	modified:   test_https/cmd/main.go

--- test_https/cmd/main.go
+++ test_https/cmd/main.go
@@ -51,8 +51,56 @@ func DefaultRedirectChecker(req *http.Request, via []*http.Request) error {
 	return nil
 }
 
+func PrintTLS(state *tls.ConnectionState) {
+	fmt.Printf("TLS.NegotiatedProtocol: \"%s\"\n", state.NegotiatedProtocol)
+
+	switch state.Version {
+	case tls.VersionTLS10:
+		fmt.Printf("TLS.Version: 1.0\n")
+	case tls.VersionTLS11:
+		fmt.Printf("TLS.Version: 1.1\n")
+	case tls.VersionTLS12:
+		fmt.Printf("TLS.Version: 1.2\n")
+	case tls.VersionTLS13:
+		fmt.Printf("TLS.Version: 1.3\n")
+	}
+
+	for _, cert := range state.PeerCertificates {
+		fmt.Printf("\nSerialNumber: %d\n", cert.SerialNumber)
+		fmt.Printf("CommonName: %s\n", cert.Issuer.CommonName)
+		fmt.Printf("NotBefore: %s\n", cert.NotBefore)
+		fmt.Printf("NotAfter: %s\n", cert.NotAfter)
+
+		if len(cert.DNSNames) > 0 {
+			fmt.Fprintf(os.Stdout, "DNSNames: [\n")
+			for _, dnsName := range cert.DNSNames {
+				fmt.Printf("%s\n", dnsName)
+			}
+			fmt.Printf("]\n")
+		}
+
+		if len(cert.EmailAddresses) > 0 {
+			fmt.Printf("EmailAddresses: [\n")
+			for _, email := range cert.EmailAddresses {
+				fmt.Printf("%s\n", email)
+			}
+			fmt.Printf("]\n")
+		}
+
+		if len(cert.IPAddresses) > 0 {
+			fmt.Printf("IPAddresses: [\n")
+			for _, ipAddr := range cert.IPAddresses {
+				fmt.Printf("%s\n", ipAddr)
+			}
+			fmt.Printf("]\n")
+		}
+	}
+}
+
 func main() {
 	var target = flag.String("url", "", "")
+	var showBody = flag.Bool("body", false, "")
+	var showHeaders = flag.Bool("headers", false, "")
 	flag.Parse()
 
 	if req, err := http.NewRequest(http.MethodGet, *target, nil); err != nil {
@@ -73,54 +121,28 @@ func main() {
 
 		} else {
 			defer resp.Body.Close()
-			fmt.Printf("%s: %s %d\n", resp.Request.Method, resp.Request.URL, resp.StatusCode)
-			fmt.Printf("Body: ")
+			fmt.Printf("%d - %s %s\n", resp.StatusCode, resp.Request.Method, resp.Request.URL)
 
-			if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
-				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			if *showHeaders {
+				fmt.Printf("\n")
+				for key := range resp.Header {
+					fmt.Printf("%s: %s\n", key, resp.Header.Get(key))
+				}
+				fmt.Printf("\n")
+			}
 
-			} else {
-				fmt.Printf("\n\n")
+			if *showBody {
+				fmt.Printf("Body:\n")
+				if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+					fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+
+				} else {
+					fmt.Printf("\n\n")
+				}
 			}
 
 			if resp.TLS != nil {
-				fmt.Printf("TLS.NegotiatedProtocol: \"%s\"\n", resp.TLS.NegotiatedProtocol)
-
-				switch resp.TLS.Version {
-				case tls.VersionTLS10:
-					fmt.Printf("TLS.Version: 1.0\n")
-				case tls.VersionTLS11:
-					fmt.Printf("TLS.Version: 1.1\n")
-				case tls.VersionTLS12:
-					fmt.Printf("TLS.Version: 1.2\n")
-				case tls.VersionTLS13:
-					fmt.Printf("TLS.Version: 1.3\n")
-				}
-
-				for _, cert := range resp.TLS.PeerCertificates {
-					fmt.Printf("SerialNumber: %d\n", cert.SerialNumber)
-					fmt.Printf("CommonName: %s\n", cert.Issuer.CommonName)
-					fmt.Printf("NotBefore: %s\n", cert.NotBefore)
-					fmt.Printf("NotAfter: %s\n", cert.NotAfter)
-
-					fmt.Fprintf(os.Stdout, "DNSNames: [\n")
-					for _, dnsName := range cert.DNSNames {
-						fmt.Printf("%s\n", dnsName)
-					}
-					fmt.Printf("]\n")
-
-					fmt.Printf("EmailAddresses: [\n")
-					for _, email := range cert.EmailAddresses {
-						fmt.Printf("%s\n", email)
-					}
-					fmt.Printf("]\n")
-
-					fmt.Printf("IPAddresses: [\n")
-					for _, ipAddr := range cert.IPAddresses {
-						fmt.Printf("%s\n", ipAddr)
-					}
-					fmt.Printf("]\n")
-				}
+				PrintTLS(resp.TLS)
 			}
 		}
 	}
