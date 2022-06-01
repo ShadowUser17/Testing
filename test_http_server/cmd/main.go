778941f3e930c3cdb2d358c7b81a75380199fd64
ShadowUser17 <linuxforce17@gmail.com>
2022-06-02 00:42:40 +0300

    Changes to be committed: 	new file:   test_http_server/cmd/main.go 	new file:   test_http_server/go.mod

---
+++ test_http_server/cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ShadowUser17/httprouter"
+)
+
+func Handler(rw http.ResponseWriter, req *http.Request, out *log.Logger) {
+	rw.WriteHeader(http.StatusOK)
+	out.Printf("%s %s", req.Method, req.URL)
+}
+
+func main() {
+	var mux = httprouter.New(nil)
+	mux.Head("/", Handler)
+	mux.Get("/", Handler)
+
+	var out = mux.GetLogger()
+	var srv = http.Server{
+		Addr:     ":8080",
+		Handler:  mux,
+		ErrorLog: out,
+	}
+
+	out.Fatal(srv.ListenAndServe())
+}
---
+++ test_http_server/go.mod
@@ -0,0 +1,5 @@
+module test_http_server
+
+go 1.17
+
+require github.com/ShadowUser17/httprouter v0.1.0
