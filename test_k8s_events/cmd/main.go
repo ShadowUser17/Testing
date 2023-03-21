0cdb171badebe06efc9d789d6ceaaf0b11388169
ShadowUser17 <linuxforce17@gmail.com>
2023-03-22 00:37:24 +0200

    Changes to be committed: 	modified:   test_k8s_events/cmd/main.go 	modified:   test_k8s_events/go.mod

--- test_k8s_events/cmd/main.go
+++ test_k8s_events/cmd/main.go
@@ -1,16 +1,73 @@
 package main
 
 import (
-	_ "context"
-	_ "flag"
-	_ "fmt"
-	_ "os"
-	_ "time"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
 
-	_ "k8s.io/apimachinery/pkg/apis/meta/v1"
-	_ "k8s.io/client-go/kubernetes"
-	_ "k8s.io/client-go/rest"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
+	var (
+		kubeconfig      = filepath.Join(homedir.HomeDir(), ".kube", "config")
+		config          *rest.Config
+		clientset       *kubernetes.Clientset
+		informerFactory informers.SharedInformerFactory
+		stopFactory     = make(chan struct{})
+		podInformer     cache.SharedIndexInformer
+		err             error
+	)
+
+	if _, err := os.Stat(kubeconfig); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if config, err = clientcmd.BuildConfigFromFlags("", kubeconfig); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if clientset, err = kubernetes.NewForConfig(config); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	informerFactory = informers.NewSharedInformerFactory(clientset, time.Second*30)
+	podInformer = informerFactory.Core().V1().Pods().Informer()
+	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			if pod, ok := obj.(*v1.Pod); ok {
+				fmt.Printf("Added: %s/%s\n", pod.Namespace, pod.Name)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			if pod, ok := obj.(*v1.Pod); ok {
+				fmt.Printf("Deleted: %s/%s\n", pod.Namespace, pod.Name)
+			}
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			if pod, ok := oldObj.(*v1.Pod); ok {
+				fmt.Printf("Deleted: %s/%s\n", pod.Namespace, pod.Name)
+			}
+
+			if pod, ok := newObj.(*v1.Pod); ok {
+				fmt.Printf("Added: %s/%s\n", pod.Namespace, pod.Name)
+			}
+		},
+	})
+
+	informerFactory.Start(stopFactory)
+	defer close(stopFactory)
+	for {
+		time.Sleep(time.Second)
+	}
 }
