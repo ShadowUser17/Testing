2853aeeda4bf0e157a7837c98e65dcdbaf037194
ShadowUser17 <linuxforce17@gmail.com>
2023-06-30 11:47:26 +0300

    Changes to be committed: 	modified:   test_aws_secret_manager/main.py

--- test_aws_secret_manager/main.py
+++ test_aws_secret_manager/main.py
@@ -8,7 +8,7 @@ import traceback
 # AWS_SECRET_ACCESS_KEY
 
 
-def list_secrets(client: any, items: int) -> list:
+def list_secrets(client: any, items: int = 30) -> list:
     resp = client.list_secrets(MaxResults=items)
     return resp.get('SecretList', [])
 
@@ -18,8 +18,13 @@ def create_secret(client: any, name: str, data: str) -> tuple:
     return (resp['Name'], resp['ARN'])
 
 
-def delete_secret(client: any, name: str) -> tuple:
-    resp = client.delete_secret(SecretId=name)
+def delete_secret(client: any, name: str, recovery: int = 30) -> tuple:
+    resp = client.delete_secret(SecretId=name, RecoveryWindowInDays=recovery)
+    return (resp['Name'], resp['DeletionDate'])
+
+
+def restore_secret(client: any, name: str) -> tuple:
+    resp = client.restore_secret(SecretId=name)
     return (resp['Name'], resp['ARN'])
 
 
@@ -28,19 +33,20 @@ def get_value(client: any, name: str) -> str:
     return resp.get('SecretString', '')
 
 
-def set_value(client: any, name: str, data: str) -> str:
+def set_value(client: any, name: str, data: str) -> tuple:
     resp = client.put_secret_value(SecretId=name, SecretString=data)
     return (resp['Name'], resp['ARN'])
 
 
 try:
     client = boto3.client('secretsmanager')
-    name = 'testing/data_v2'
+    name = 'testing/data'
     #print(create_secret(client, name, 'testing...'))
     #print(get_value(client, name))
     #print(set_value(client, name, '1..2..3..'))
     #print(get_value(client, name))
     #print(delete_secret(client, name))
+    #print(restore_secret(client, name))
 
 except Exception:
     traceback.print_exc()
