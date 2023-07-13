e676d73e58251abe27094a34213a0952c993172f
ShadowUser17 <linuxforce17@gmail.com>
2023-07-13 14:36:15 +0300

    Changes to be committed: 	modified:   test_loki_push_logs/cmd/main.go

--- test_loki_push_logs/cmd/main.go
+++ test_loki_push_logs/cmd/main.go
@@ -1,10 +1,45 @@
 package main
 
 import (
-	_ "encoding/json"
-	_ "fmt"
-	_ "strings"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
 )
 
+type Logger struct {
+	Loki   string
+	Labels map[string]string
+}
+
+func (log *Logger) Push(message string) (*http.Response, error) {
+	var rawLabels = make([]string, 0)
+	for key, val := range log.Labels {
+		rawLabels = append(rawLabels, fmt.Sprintf("\"%s\": \"%s\"", key, val))
+	}
+
+	var fmtData = strings.NewReader(fmt.Sprintf(
+		"{\"streams\": [{\"stream\": {%s}, \"values\": [[\"%d\", \"%s\"]] }]}",
+		strings.Join(rawLabels, ", "), time.Now().UnixNano(), message,
+	))
+
+	return http.Post(log.Loki, "application/json", fmtData)
+}
+
 func main() {
+	var labels = make(map[string]string)
+	labels["env"] = "testing"
+	labels["service"] = "backend"
+
+	var logger = Logger{
+		Loki:   "http://grafana-loki.k3s/loki/api/v1/push",
+		Labels: labels,
+	}
+
+	var resp, err = logger.Push("Testing...")
+	defer resp.Body.Close()
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
 }
