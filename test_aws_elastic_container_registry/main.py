ebb573459934d5dc19b346db08bafb5b9cc6d5d4
ShadowUser17 <linuxforce17@gmail.com>
2023-07-27 18:07:56 +0300

    Changes to be committed: 	new file:   test_aws_elastic_container_registry/README.md 	new file:   test_aws_elastic_container_registry/main.py 	new file:   test_aws_elastic_container_registry/requirements.txt

---
+++ test_aws_elastic_container_registry/main.py
@@ -0,0 +1,34 @@
+import traceback
+import base64
+import boto3
+
+# AWS_DEFAULT_REGION
+# AWS_PROFILE
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+
+def get_auth_token(client: any) -> list:
+    '''
+    return ["user", "password"]
+    '''
+    responce = client.get_authorization_token()
+    tmp = base64.b64decode(responce["authorizationData"][0]["authorizationToken"])
+    return tmp.decode().split(":")
+
+
+def describe_repos(client: any, repo_list: list) -> list:
+    responce = client.describe_repositories(repositoryNames=repo_list)
+    return responce["repositories"]
+
+
+def get_repo_tags(client: any, arn: str) -> list:
+    responce = client.list_tags_for_resource(resourceArn=arn)
+    return responce["tags"]
+
+
+try:
+    client = boto3.client("ecr")
+    print(get_auth_token(client))
+
+except Exception:
+    traceback.print_exc()
