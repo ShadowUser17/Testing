11158f21cff23fdc088655b9a51e890667c6c2be
ShadowUser17 <linuxforce17@gmail.com>
2023-08-10 19:29:48 +0300

    Changes to be committed: 	modified:   test_github_api/main.py

--- test_github_api/main.py
+++ test_github_api/main.py
@@ -4,22 +4,30 @@ import traceback
 import json
 import os
 
+CMD_GH_PATH = git = os.path.join(os.path.expanduser("~"), "gh")
 
-def get_forks() -> list:
-    git = os.path.join(os.path.expanduser("~"), "gh")
+
+def list_forks() -> list:
     cmd = subprocess.Popen(
-        [git, "repo", "list", "--fork", "--json", "name"],
-        shell=False,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.DEVNULL
+        [CMD_GH_PATH, "repo", "list", "--fork", "--json", "nameWithOwner,defaultBranchRef"],
+        shell=False, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL
     )
 
     (out, _) = cmd.communicate()
-    return list(map(lambda item: item["name"], json.loads(out)))
+    return list(map(lambda item: [item["nameWithOwner"], item["defaultBranchRef"]["name"]], json.loads(out)))
+
+
+def sync_fork(repo_name: str, repo_branch: str) -> int:
+    cmd = subprocess.Popen(
+        [CMD_GH_PATH, "repo", "sync", repo_name, "--force", "--branch", repo_branch], shell=False
+    )
+
+    return cmd.wait()
 
 
 try:
-    print(get_forks())
+    for (name, branch) in list_forks():
+        sync_fork(name, branch)
 
 except Exception:
     traceback.print_exc()
