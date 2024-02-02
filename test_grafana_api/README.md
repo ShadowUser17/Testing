e9d526d83e080d5db216130a148a0493509e608f
ShadowUser17 <linuxforce17@gmail.com>
2024-02-02 14:57:29 +0200

    Changes to be committed: 	new file:   test_grafana_api/README.md 	new file:   test_grafana_api/main.py

---
+++ test_grafana_api/README.md
@@ -0,0 +1,2 @@
+#### URLs:
+- [API](https://grafana.com/docs/grafana/latest/developers/http_api/)
---
+++ test_grafana_api/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+import traceback
+
+from urllib import request
+from urllib import parse as urllib
+
+try:
+    pass
+
+except Exception:
+    traceback.print_exc()
+    sys.exit(1)
