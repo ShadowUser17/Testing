37f88e54a8493005f92e7c45b61f43dc5173e3fe
ShadowUser17 <linuxforce17@gmail.com>
2024-02-11 00:59:12 +0200

    Changes to be committed: 	modified:   test_grafana_api/main.py

--- test_grafana_api/main.py
+++ test_grafana_api/main.py
@@ -23,18 +23,20 @@ class Grafana:
         return str(urllib.urljoin(self._url, path))
 
     def _request(self, req: request.Request) -> dict:
+        req.headers = self._headers
         ctx = None if self._verify else ssl._create_unverified_context()
 
         with request.urlopen(url=req, context=ctx) as client:
             return json.loads(client.read())
 
+    # https://grafana.com/docs/grafana/latest/developers/http_api/folder/#get-all-folders
     def list_folders(self) -> dict:
         return self._request(request.Request(
             method="GET",
-            url=self._mkurl("/api/folders"),
-            headers=self._headers
+            url=self._mkurl("/api/folders")
         ))
 
+    # https://grafana.com/docs/grafana/latest/developers/http_api/folder/#create-folder
     def create_folder(self, name: str) -> dict:
         data = json.dumps({
             "uid": str(uuid.uuid1()),
@@ -44,15 +46,21 @@ class Grafana:
         return self._request(request.Request(
             method="POST",
             url=self._mkurl("/api/folders"),
-            data=data.encode(),
-            headers=self._headers
+            data=data.encode()
         ))
 
+    # https://grafana.com/docs/grafana/latest/developers/http_api/folder/#delete-folder
     def delete_folder(self, uid: str) -> dict:
         return self._request(request.Request(
             method="DELETE",
-            url=self._mkurl("/api/folders/{}".format(uid)),
-            headers=self._headers
+            url=self._mkurl("/api/folders/{}".format(uid))
+        ))
+
+    # https://grafana.com/docs/grafana/latest/developers/http_api/data_source/#get-all-data-sources
+    def list_datasources(self) -> dict:
+        return self._request(request.Request(
+            method="GET",
+            url=self._mkurl("/api/datasources")
         ))
 
 
@@ -61,7 +69,7 @@ try:
     print(client.list_folders())
     #print(client.create_folder("dmytro_test"))
     #print(client.delete_folder("9890a252-c865-11ee-a045-d8bbc1905606"))
-    #print(client.list_folders())
+    #print(client.list_datasources())
 
 except Exception:
     traceback.print_exc()
