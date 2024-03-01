1954aab5e23dfec12e21799646345c5d58719226
ShadowUser17 <linuxforce17@gmail.com>
2024-03-01 16:37:57 +0200

    Changes to be committed: 	new file:   test_k8s_python/README.md 	modified:   test_k8s_python/main.py 	modified:   test_k8s_python/requirements.txt

--- test_k8s_python/main.py
+++ test_k8s_python/main.py
@@ -1,19 +1,26 @@
-#!/usr/bin/env python3
 # https://github.com/kubernetes-client/python
 import sys
-from traceback import print_exc
+import logging
+import traceback
+
 from kubernetes import config as k8s_config
 from kubernetes import client as k8s_client
 
 
 try:
+    logging.basicConfig(
+        format=r'%(levelname)s [%(asctime)s]: "%(message)s"',
+        datefmt=r'%Y-%m-%d %H:%M:%S',
+        level=logging.INFO
+    )
+
     k8s_config.load_kube_config()
     core_api = k8s_client.CoreV1Api()
 
     res = core_api.list_pod_for_all_namespaces(watch=False)
     for item in res.items:
-        print("{}/{}/{}".format(item.spec.node_name, item.metadata.namespace, item.metadata.name))
+        logging.info("{}/{}/{}".format(item.spec.node_name, item.metadata.namespace, item.metadata.name))
 
 except Exception:
-    print_exc()
+    logging.error(traceback.format_exc())
     sys.exit(1)
