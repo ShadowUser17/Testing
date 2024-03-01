75af53367f1d57f06cf9a37c3c662696d5a688dc
ShadowUser17 <linuxforce17@gmail.com>
2024-03-01 16:53:02 +0200

    Changes to be committed: 	modified:   test_k8s_python/README.md 	modified:   test_k8s_python/main.py

--- test_k8s_python/README.md
+++ test_k8s_python/README.md
@@ -3,3 +3,8 @@
 python3 -m venv --upgrade-deps env && \
 ./env/bin/pip3 install -r requirements.txt
 ```
+
+#### Run interactive shell:
+```bash
+./env/bin/python3 -i main.py
+```
--- test_k8s_python/main.py
+++ test_k8s_python/main.py
@@ -1,26 +1,15 @@
 # https://github.com/kubernetes-client/python
-import sys
 import logging
-import traceback
 
 from kubernetes import config as k8s_config
 from kubernetes import client as k8s_client
 
 
-try:
-    logging.basicConfig(
-        format=r'%(levelname)s [%(asctime)s]: "%(message)s"',
-        datefmt=r'%Y-%m-%d %H:%M:%S',
-        level=logging.INFO
-    )
+logging.basicConfig(
+    format=r'%(levelname)s [%(asctime)s]: "%(message)s"',
+    datefmt=r'%Y-%m-%d %H:%M:%S',
+    level=logging.INFO
+)
 
-    k8s_config.load_kube_config()
-    core_api = k8s_client.CoreV1Api()
-
-    res = core_api.list_pod_for_all_namespaces(watch=False)
-    for item in res.items:
-        logging.info("{}/{}/{}".format(item.spec.node_name, item.metadata.namespace, item.metadata.name))
-
-except Exception:
-    logging.error(traceback.format_exc())
-    sys.exit(1)
+k8s_config.load_kube_config()
+client = k8s_client.CoreV1Api()
