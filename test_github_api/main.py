d2bc18682886821558752936da77a60607414427
ShadowUser17 <linuxforce17@gmail.com>
2024-05-23 15:11:05 +0300

    Update test_github_api/main.py

--- test_github_api/main.py
+++ test_github_api/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import github
+import datetime
 import traceback
 
 
@@ -8,6 +9,7 @@ try:
     github_token = os.environ.get("GITHUB_TOKEN")
     github_org = os.environ.get("GITHUB_ORG")
 
+    now = datetime.datetime.now()
     auth = github.Auth.Token(github_token)
     client = github.Github(auth=auth)
 
@@ -19,9 +21,11 @@ try:
     for repo_item in repo_list:
         print("Repository:", repo_item.name)
 
-        repo_runs = repo_item.get_workflow_runs(status="queued")
+        repo_runs = repo_item.get_workflow_runs(status="completed", created=now.strftime(r"%Y-%m-%d"))
         for run_item in repo_runs:
-            print("\t{}: {}".format(run_item.name, run_item.status))
+            print("\t\"{}\" \"{}\": {}".format(
+                run_item.created_at.strftime(r"%Y-%m-%d %H:%M:%S"), run_item.name, run_item.status.capitalize()
+            ))
 
 except Exception:
     traceback.print_exc()
