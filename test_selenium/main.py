69469e2ea3342dadf7d77be82777e8ed3f243de0
ShadowUser17 <linuxforce17@gmail.com>
2025-06-06 14:36:24 +0300

    Changes to be committed: 	modified:   test_selenium/main.py

--- test_selenium/main.py
+++ test_selenium/main.py
@@ -10,16 +10,28 @@ from selenium.webdriver.support import expected_conditions
 
 def get_driver() -> any:
     chrome_options = Options()
+    chrome_options.add_argument("--incognito")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--no-first-run")
+    chrome_options.add_argument("--disable-default-apps")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-popup-blocking")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--start-maximized")
+
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+
     return webdriver.Remote(
-        command_executor=os.environ.get("SELENIUM_URL", "http://selenium.docker:4444/wd/hub"),
+        command_executor=os.environ.get("SELENIUM_URL", "http://selenium.docker/wd/hub"),
         options=chrome_options
     )
 
 driver = get_driver()
-driver.set_window_size(1280, 720)
 print("Start session:", driver.session_id)
 
 # driver.get("https://www.google.com/")
